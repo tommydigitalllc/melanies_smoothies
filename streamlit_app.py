f816d15a5ff736f0f0425e464e728f593938f03d
tommydigitalllc <tommy@tommydigitalllc.com>
2024-06-25 20:04:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -48,5 +49,5 @@ if ingredients_list:
 
 
 
-import requests
+
 
